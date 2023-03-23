c2d1a6ee3d6c788a19dcd780e2bc642e694a4877
VIJAY KUMAR <127007280+vijay-kumar834@users.noreply.github.com>
2023-03-23 12:47:34 +0000

    Set options to make browsing easier

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-def audio():
-  if input == 1:
-    return 5
-  else:
-    return False
+from selenium import webdriver
 
-print(audio())
+# Set options to make browsing easier
+options = webdriver.ChromeOptions()
+options.add_argument("disable-infobars")
+options.add_argument("start-maximized")
+options.add_argument("diable-dev-shm-usage")
+options.add_argument("no-sandbox")
+options.add_experimental_option("excludeSwitches",["enable-automation"])
+options.add_argument("disable-blink-features=AutomationControlled")
+
+
+driver = webdriver.Chrome(options)
+
